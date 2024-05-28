4c6a318d104b6535f133f7066faa85d28ac2017b
lucascerdeira <devlucascerdeira@gmail.com>
2024-05-27 22:10:35 -0300

    iniciando projeto todo-list com spring boot

--- todolist/src/main/java/br/com/lucascerdeira/todolist/TodolistApplication.java
+++ todolist/src/main/java/br/com/lucascerdeira/todolist/TodolistApplication.java
@@ -9,7 +9,7 @@ public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-		// stat da aplicação
+		// stat da aplicação here
 	}
 
 }
