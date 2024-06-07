ec972339cc7ce0479b9c20c081d194001c15021f
Lucas Almeida Cerdeira <lukscerdeira96@gmail.com>
2024-06-06 21:28:45 -0300

    protect routes

--- todolist/src/main/java/br/com/lucascerdeira/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/lucascerdeira/todolist/task/TaskController.java
@@ -1,10 +1,16 @@
 package br.com.lucascerdeira.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,11 +19,18 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping("/task")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        System.out.println(idUser);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt())){
+
+        }
         var task = this.taskRepository.save(taskModel);
         
-        return task ;
+        return new ResponseEntity<>(null);
         
 
     }
